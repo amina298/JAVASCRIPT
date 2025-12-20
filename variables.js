d1344d5bc6a8c0bb334df283df7797a4c6619772
Amina <aminajama219@gmail.com>
2025-12-20 03:51:17 -0800

    added variables

---
+++ variables.js
@@ -0,0 +1,34 @@
+var firstName = 'Amina';
+var lastName = 'jama';
+var fullName = firstName + " "  + lastName;
+console.log(fullName)
+// number data type
+let year = 2020;
+ year = 2015;
+console.log(year);
+
+//  u can reassign and redeclare with var 
+var age = 20;
+age = 30;
+var age = 50;
+var result = age; 
+console.log(result);
+
+// null data type
+
+// null is  empty on purpose
+var age = null;
+age = 25;
+console.log(age); 
+
+var email = 'aminajama219@gmail.com';
+email = null;
+console.log(email); 
+
+
+
+
+
+
+
+
