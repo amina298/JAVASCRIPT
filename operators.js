6bf3be780c139c5515083e095dd8a1ffb8546c4e
Amina <aminajama219@gmail.com>
2025-12-20 08:16:58 -0800

    added operators

---
+++ operators.js
@@ -0,0 +1,75 @@
+// Aithmetic operator
+
+// Addition
+var a = 10;
+var b = 20;
+var sum = a + b;
+console.log(sum);
+
+// subtraction
+var s = 80;
+var t = 30;
+var result = s - t;
+console.log(result);
+
+// Multiplication
+var price = 40;
+var quantity = 20;
+var total = price * quantity;
+console.log(total);
+
+
+// Division
+var totalMarks = 100;
+var students = 4;
+var average = totalMarks / students;
+console.log(average);
+
+
+// Modulus
+var number = 10;
+var remainder = number % 3;
+
+console.log(remainder);
+
+// reassigning with arithmetics
+var score = 10;
+score = score + 5;
+console.log(score);
+
+// this is the shorthand of the above same output
+var score = 10;
+score += 5;
+console.log(score);
+ 
+// Comparison operators
+var a = 5;
+var b = '5';
+console.log(a == b);
+// == is true by defauly
+
+var g = '7';
+var v = 10 
+console.log(v === g);
+// false bcz 3 things are being compared first value + second value + data type
+
+ var a = 5;
+ var b = 10;
+
+ console.log(a != b); 
+// ! means not equal
+
+
+var n = 75;
+var l = 60;
+console.log(n > l);
+// greater than
+
+
+
+// AND &&   || OR   ! NOT  
+let x = 10;
+let y = 20;
+console.log(x < y && x > 5);
+console.log(x < y || x > 25);
+console.log(!(x > y));
